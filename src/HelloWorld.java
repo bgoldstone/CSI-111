9efa6ced47ca2f5d7783a3fef2c04d834af281b2
bgoldstone <bgoldstone1@gmail.com>
2021-02-10 10:37:00 -0500

    javadoc

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,8 @@
-/*
-* @author BenGoldstone
-* @date
-* */
+/**
+ * @author Ben Goldstone
+ * @version 1.0
+ *
+ */
 public class HelloWorld {
     public static void main(String[] args) {
     System.out.println("Hello World!");
