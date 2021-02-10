fa7d69fc8bbae209480d2f79304f3725e1ecb346
bgoldstone <bgoldstone1@gmail.com>
2021-02-10 10:43:52 -0500

    adjust formatting

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,9 @@
 /**
  * @author Ben Goldstone
  * @version 1.0
- *
  */
 public class HelloWorld {
     public static void main(String[] args) {
-    System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
