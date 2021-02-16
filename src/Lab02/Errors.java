e1a8b6d164955c36c5700b3b37805e9251ebd635
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-02-16 12:44:53 -0500

    Reformatted Code

--- src/Lab02/Errors.java
+++ src/Lab02/Errors.java
@@ -11,14 +11,14 @@ package Lab02;
 //missing class keywoard
 public class Errors {
     //missing args parameter and static
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //missing semicolon
         System.out.println("This code contains multiple errors.");
         //uses a " ' instead of ""
         System.out.print("These need to be fixed before you submit it.");
         //missing uses pow method but does not define Math class
-        System.out.println(Math.pow(2,4));
+        System.out.println(Math.pow(2, 4));
         //spelled out as our
         System.out.println("Fixed by Ben Goldstone.");
-        }
+    }
 }
