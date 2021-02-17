60b52a5b17e8acc66599b05366cd92fbca0d281d
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-02-17 10:37:06 -0500

    Changed back to regular Switch Statement

--- src/Lab02/Switch.java
+++ src/Lab02/Switch.java
@@ -30,9 +30,18 @@ public class Switch {
         //Scans Choice
         choice = scan.next().charAt(0);
         switch (choice) {
-            case 'A', 'B', 'D', 'E' -> System.out.println("That is incorrect! Neptune is the planted farthest away from the sun.");
-            case 'C' -> System.out.println("That is correct! Neptune is the planted farthest away from the sun.");
-            default -> System.out.println("That is not a valid choice.");
+            case 'A':
+            case 'B':
+            case 'D':
+            case 'E':
+                System.out.println("That is incorrect! Neptune is the planted farthest away from the sun.");
+                break;
+            case 'C':
+                System.out.println("That is correct! Neptune is the planted farthest away from the sun.");
+                break;
+            default:
+                System.out.println("That is not a valid choice.");
+                break;
         }
         System.out.println("Good Bye");
     }
