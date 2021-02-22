bc44c3f600642238216cf706699cfb375c43d798
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-02-22 10:15:26 -0500

    Updated Null

--- src/Null.java
+++ src/Null.java
@@ -2,5 +2,6 @@ public class Null {
     public static void main(String[] args) {
         int dollar = 120;
         System.out.println(dollar%'a');
+        //bit operators System.out.println(0&1);
     }
 }
