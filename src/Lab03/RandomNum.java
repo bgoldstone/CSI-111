a3dcdf3dbd1bd8607ac8a2b28aed8901f5709f25
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-02-24 10:25:14 -0500

    pushed into method instead of in main

--- src/Lab03/RandomNum.java
+++ src/Lab03/RandomNum.java
@@ -1,4 +1,5 @@
 package Lab03;
+import java.util.Random;
 
 public class RandomNum {
     public static void main(String[] args) {
@@ -14,40 +15,34 @@ public class RandomNum {
         final int RANDINT2MIN = 11;
 
         //generates random integer minimum for set #1
-        int randInt1 = (int) (Math.random() * RANGE + RANDINT1MIN);
-        System.out.println("\nrandInt1: " + randInt1);
 
-        //generates random integer minimum for set #2
-        int randInt2 = (int) (Math.random() * RANGE + RANDINT2MIN);
-        System.out.println("randInt2: " + randInt2);
-
-        //initializes sum variables
-        int randInt1Sum = 0, randInt2Sum = 0;
-
-        //number set #1
-        System.out.print("\nNum Set #1: \n[");
-        for (int i = randInt1; i <= (RANDINT1MIN + RANGE); i += 2) {
-            if (i % 2 != 0) {
-                i++;
-            }
-            System.out.print(i + ", ");
-            randInt1Sum += i;
-
-        }
-        System.out.println("]");
-        //number set #2
-        System.out.print("\nNum Set #2:\n[");
-        for (int i = randInt2; i <= (RANDINT2MIN + RANGE); i += 2) {
-            if (i % 2 != 0) {
-                i++;
-            }
-            System.out.print(i + ", ");
-            randInt2Sum += i;
-
-        }
-        System.out.println("]");
+        int randInt1Sum = randInt(RANGE,RANDINT1MIN);
+        int randInt2Sum = randInt(RANGE,RANDINT2MIN);
 
         //print results
         System.out.println("\n\nRandom Number Set #1 Sum: " + randInt1Sum + "\nRandom Number Set #2 Sum: " + randInt2Sum);
+    }
+
+    /**
+     * Generates a list of random integers and adds them all together
+     * @param range determines range of numbers.
+     * @param minimum determines starting value
+     * @return sum returns sum of minimum value plus the even numbers up to the range.
+     */
+    public static int randInt(int range, int minimum){
+        Random rand = new Random();
+        int randomInteger = (rand.nextInt(range) + minimum);
+        int sum = 0;
+        System.out.println("\nrandInt: " + randomInteger);
+        System.out.print("\nNum Set \n[");
+        for (int i = randomInteger; i <= (minimum + range); i += 2) {
+            if (i % 2 != 0) {
+                i++;
+            }
+            System.out.print(i + ", ");
+            sum += i;
+        }
+            System.out.println("]");
+        return sum;
     }
 }
