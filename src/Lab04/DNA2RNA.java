fa2491f1e8e928928a3cbf318d4fbd06eb9dd478
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-03-01 18:44:52 -0500

    DNA2RNA Complete??

--- src/Lab04/DNA2RNA.java
+++ src/Lab04/DNA2RNA.java
@@ -1,7 +1,51 @@
 package Lab04;
 
+import java.util.Scanner;
+
 public class DNA2RNA {
     public static void main(String[] args) {
+        String DNA = getDNA();
+        String RNA = toRNA(DNA);
+        System.out.println("The DNA sequence " + DNA + " is " + RNA + " in RNA");
+    }
+
+    /**
+     * A method to get user DNA input and validates it.
+     *
+     * @return DNA in a String
+     */
+    public static String getDNA() {
+        Scanner input = new Scanner(System.in);
+        String dna;
+        boolean flag;
+        do {
+            flag = false;
+            System.out.println("Please enter a series of DNA to be converted to RNA");
+            dna = input.nextLine().toUpperCase().strip();
+            for (char c : dna.toCharArray()) {
+                if (c != 'A' && c != 'C' && c != 'G' && c != 'T') {
+                    flag = true;
+                }
+            }
+        } while (flag);
+        return dna;
+    }
+
+    /**
+     *
+     * @param dna the String of DNA to be converted to RNA
+     * @return String of DNA converted to RNA
+     */
+    public static String toRNA(String dna){
+        StringBuilder rna = new StringBuilder(dna);
+        for (int i = 0; i < rna.length(); i++) {
+            if(rna.charAt(i) == 'T'){
+                rna.setCharAt(i,'U');
+            }
+
+        }
+        return rna.toString();
 
     }
+
 }
