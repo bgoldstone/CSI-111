f7a4001128d4b9b2f463e49d54944c5ec753d10a
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-03-22 11:55:56 -0400

    Did 1st part of Lab06

---
+++ src/OrderSystem.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+/**
+ * @author Ben Goldstone
+ * @version 3/26/21
+ * Instructor: Professor Helsing
+ * Description: Takes users order information including name, number of units,
+ * and tells user total cost
+ */
+public class OrderSystem {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Product product = new Product();
+        System.out.println("Welcome to the Product Ordering System!");
+        System.out.print("What is the name of the product you want to order? ");
+        product.setName(scan.nextLine());
+        System.out.printf("How many units of %s do you want? ", product.getName());
+        product.setNumberOfUnits(scan.nextInt());
+        product.setPrice(9.99);
+        calcTotal(product);
+        System.out.println("\n####################");
+        System.out.printf("Name: %s", product.getName());
+        System.out.printf("\nUnits: %d", product.getNumberOfUnits());
+        System.out.printf("\nPrice per Unit: %.2f", product.getPrice());
+        System.out.printf("\nTotal Price: %.2f", product.getTotalCost());
+        System.out.println("\n####################");
+    }
+
+    /**
+     * Calculates total cost depending on price and units
+     * @param product takes an object of Product
+     */
+    private static void calcTotal(Product product){
+        product.setTotalCost(product.getPrice()*product.getNumberOfUnits());
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,69 @@
+/**
+ * @author Ben Goldstone
+ * @version 3/26/21
+ * Instructor: Professor Helsing
+ * Description: contains the structure for a product object.
+ */
+
+public class Product {
+    private String name; //name of product
+    private int numberOfUnits; //number of units the customer wants to order
+    private double price; //price of one unit of product
+    private double totalCost; //total cost of order
+
+    /**
+     * @return Gets name of item
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name Sets name of item
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return Gets number of units customer wants to order
+     */
+    public int getNumberOfUnits() {
+        return numberOfUnits;
+    }
+
+    /**
+     * @param numberOfUnits Sets number of units customer wants to order
+     */
+    public void setNumberOfUnits(int numberOfUnits) {
+        this.numberOfUnits = numberOfUnits;
+    }
+
+    /**
+     * @return Gets price of item
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price Sets price of item
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * @return Gets total cost of customer's order
+     */
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    /**
+     * @param totalCost Sets total cost of customer's order
+     */
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+}
