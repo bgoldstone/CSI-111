9af0b5f4654a4c2870f38924923e4621fc35280a
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-04-05 13:21:41 -0400

    Started Poem.java

---
+++ Lab08/Poem.java
@@ -0,0 +1,13 @@
+/**
+ * @author Benjamin Goldstone (bgoldstone@muhlenberg.edu)
+ * @version 4/9/2021
+ */
+/*
+    Instructor: Professor Joseph Helsing
+    Description:
+ */
+public class Poem {
+    public static void main(String[] args) {
+
+    }
+}
