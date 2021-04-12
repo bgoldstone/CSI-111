5a9ad6725bcc748760d696e16d9843875e2fe8c1
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-04-12 15:06:56 -0400

    Commenting!!

--- Lab09/TypeChecker.java
+++ Lab09/TypeChecker.java
@@ -1,37 +1,64 @@
 import java.util.Scanner;
 
+/**
+ * Tells user what data type is inserted in the scanner
+ *
+ * @author Ben Goldstone
+ */
+/*
+    Instructor: Professor Joseph Helsing
+    Date: 4/16/2021
+ */
 public class TypeChecker {
-    private static int integerValue;
-    private static double doubleValue;
+    private static int integerValue; //Stores integer value.
+    private static double doubleValue; //Stores double value.
 
     public static void main(String[] args) {
+        //Stores Scanner object.
         Scanner scan = new Scanner(System.in);
+
+        //Prompts user for input.
         System.out.print("Type in a value: ");
         String line = scan.nextLine();
         System.out.println();
+
+        //If is integer, tell user.
         if (tryInt(line)) {
-            System.out.printf("The value %s is integer data\n", line);
+            System.out.printf("The value %d is integer data\n", integerValue);
+
+        //If is double, tell user.
         } else if (tryDouble(line)) {
-            System.out.printf("The value %s is double data\n", line);
+            System.out.printf("The value %f is double data\n", doubleValue);
+        //If no data besides whitespaces, tell user.
         } else if (line.trim().length() == 0) {
-            System.out.printf("The value %s is only whitespace data\n", line);
+            System.out.printf("The value \"%s\" is only whitespace data\n", line);
+        //Else, it must be text.
         } else {
-            System.out.printf("The value %s is text data\n", line);
+            System.out.printf("The value \"%s\" is text data\n", line);
         }
     }
 
-    private static boolean tryInt(String number) {
+    /**
+     * Tells user if data is integer
+     * @param text String input to test
+     * @return boolean
+     */
+    private static boolean tryInt(String text) {
         try {
-            integerValue = Integer.parseInt(number);
+            integerValue = Integer.parseInt(text);
         } catch (NumberFormatException e) {
             return false;
         }
         return true;
     }
-
-    private static boolean tryDouble(String number) {
+    /**
+     * Tells user if data is double
+     * @param text String input to test
+     * @return boolean
+     */
+    private static boolean tryDouble(String text) {
         try {
-            doubleValue = Double.parseDouble(number);
+            doubleValue = Double.parseDouble(text);
         } catch (NumberFormatException e) {
             return false;
         }
