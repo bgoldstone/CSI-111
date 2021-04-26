c201a69e5c62b314e4558a6f99f42cbfbe456521
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-04-26 12:32:28 -0400

    Started working on HRManager

---
+++ Lab10/HRManager.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HRManager {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many employees are there? ");
+        Employee[] employees = new Employee[scan.nextInt()];
+        for (int i = 0; i < employees.length; i++) {
+
+        }
+    }
+    private static double calcTotalPay(Employee[] employees){
+        return 0;
+    }
+    private static void listEmployees(Employee[] employees){
+
+    }
+}
