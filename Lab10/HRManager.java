8b98f6ab2e4707792992c41221cc19a3245a233e
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-04-26 16:06:45 -0400

    Finished HRManager

--- Lab10/HRManager.java
+++ Lab10/HRManager.java
@@ -5,44 +5,52 @@ public class HRManager {
         Scanner scan = new Scanner(System.in);
         System.out.print("How many employees are there? ");
         Employee[] employees = new Employee[scan.nextInt()];
+        scan.nextLine();
         String choice;
         String name;
         int id;
         int levelShift;
         double payRate;
         for (int i = 0; i < employees.length; i++) {
-            System.out.print("What type of user would you like? (Supervisor/Worker)");
+            System.out.print("What type of user would you like? (Supervisor/Worker) ");
             choice = scan.nextLine();
             System.out.println();
             while (!(choice.equalsIgnoreCase("Supervisor")) && !(choice.equalsIgnoreCase("Worker"))) {
-                System.out.print("Invalid user!\nWhat type of user would you like? (Supervisor/Worker)");
+                System.out.print("Invalid user!\nWhat type of user would you like? (Supervisor/Worker) ");
                 choice = scan.nextLine();
                 System.out.println();
             }
             if (choice.equalsIgnoreCase("Supervisor")) {
                 System.out.print("What is the employee's name? ");
                 name = scan.nextLine();
-                System.out.printf("\nWhat is %S's ID number? ", name);
+                System.out.printf("%nWhat is %s's ID number? ", name);
                 id = scan.nextInt();
-                System.out.printf("\nWhat is %S's supervisor level? ", name);
+                scan.nextLine();
+                System.out.printf("%nWhat is %s's pay rate? ", name);
+                payRate = scan.nextDouble();
+                scan.nextLine();
+                System.out.printf("%sWhat is %s's supervisor level? ", name);
                 levelShift = scan.nextInt();
-                System.out.printf("\nWhat is %S's pay rate? ", name);
-                payRate = scan.nextInt();
+                scan.nextLine();
                 employees[i] = new Supervisor(name, id, levelShift, payRate);
             } else if (choice.equalsIgnoreCase("Worker")) {
                 System.out.print("What is the employee's name? ");
                 name = scan.nextLine();
-                System.out.printf("\nWhat is %S's ID number? ", name);
+                System.out.printf("%sWhat is %s's ID number? ", name);
                 id = scan.nextInt();
-                System.out.printf("\nWhat is %S's shift number? (1 for day shift, 2 for night shift)", name);
+                scan.nextLine();
+                System.out.printf("%sWhat is %s's pay rate? ", name);
+                payRate = scan.nextDouble();
+                scan.nextLine();
+                System.out.printf("%sWhat is %s's shift number? (1 for day shift, 2 for night shift) ", name);
                 levelShift = scan.nextInt();
-                System.out.printf("\nWhat is %S's pay rate? ", name);
-                payRate = scan.nextInt();
+                scan.nextLine();
                 employees[i] = new BasicWorker(name, id, levelShift, payRate);
             }
+            System.out.println();
         }
         listEmployees(employees);
-        System.out.printf("\nTotal pay: %.2f", calcTotalPay(employees));
+        System.out.printf("%s%sTotal pay: %,.2f", calcTotalPay(employees));
     }
 
     private static double calcTotalPay(Employee[] employees) {
@@ -61,24 +69,24 @@ public class HRManager {
             System.out.println("\n");
             if (employee instanceof BasicWorker) {
                 worker = (BasicWorker) employee;
-                System.out.printf("Name: %S", worker.getEmployeeName());
-                System.out.printf("\nID: %d", worker.getEmployeeID());
-                System.out.printf("\nPay Rate: %.2f", worker.getWorkerPayRate());
+                System.out.printf("Name: %s", worker.getEmployeeName());
+                System.out.printf("%sID: %d", worker.getEmployeeID());
+                System.out.printf("%sPay Rate: $%,.2f", worker.getWorkerPayRate());
                 if (worker.getWorkerShift() == 1) {
                     shift = "Day Shift";
                 }
                 if (worker.getWorkerShift() == 2) {
                     shift = "Night Shift";
                 }
-                System.out.printf("\nShift: %S", shift);
+                System.out.printf("%sShift: %s", shift);
 
             }
             if (employee instanceof Supervisor) {
                 supervisor = (Supervisor) employee;
-                System.out.printf("Name: %S", supervisor.getEmployeeName());
-                System.out.printf("\nID: %d", supervisor.getEmployeeID());
-                System.out.printf("\nPay Rate: %.2f", supervisor.getSupervisorPayRate());
-                System.out.printf("\nLevel: %d", supervisor.getSupervisorLevel());
+                System.out.printf("Name: %s", supervisor.getEmployeeName());
+                System.out.printf("%sID: %d", supervisor.getEmployeeID());
+                System.out.printf("%sPay Rate: %,.2f", supervisor.getSupervisorPayRate());
+                System.out.printf("%sLevel: %d", supervisor.getSupervisorLevel());
             }
         }
     }
