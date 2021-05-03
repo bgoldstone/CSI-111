09aaff40aec7a6a59044ecb48f76de4e044bde77
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-05-03 15:53:16 -0400

    Lab11 init commit

---
+++ Lab11/FreqCountV1.java
@@ -0,0 +1,72 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+    Author: Ben Goldstone
+    Date: 5/7/2021
+    Instructor: Professor Joseph Helsing
+    Description: Counts frequency of a word count in a file.
+ */
+public class FreqCountV1 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap();
+        try {
+            readTextFile(map);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        outputFreq(map);
+    }
+
+    /**
+     * Reads text file and puts into a hash map.
+     *
+     * @param map an object of a hash map using string and integers
+     * @throws FileNotFoundException Scanner exception
+     */
+    private static void readTextFile(HashMap<String, Integer> map) throws FileNotFoundException {
+        //Initializes/Declares variables and object instances.
+        Scanner scan = new Scanner(System.in);
+        Scanner fileReader;
+        String word;
+        File file;
+
+        //Gets file name.
+        System.out.print("What is the name of the file? ");
+        file = new File(scan.nextLine());
+        System.out.println();
+        while (!file.exists()) {
+            System.out.print("Incorrect Name!\n What is the name of the file? ");
+            file = new File(scan.nextLine());
+            System.out.println();
+        }
+        fileReader = new Scanner(file);
+
+        //Reads all words in file.
+        while (fileReader.hasNext()) {
+            word = fileReader.next().toLowerCase();
+            //If already exists, add one.
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+
+                //Else create new key,value pair.
+            } else {
+                map.put(word, 1);
+            }
+        }
+    }
+
+    /**
+     * Outputs formatted hash map.
+     *
+     * @param map an object of a hash map using strings and integers
+     */
+    private static void outputFreq(HashMap<String, Integer> map) {
+        //For each key output it.
+        for (Object key : map.keySet()) {
+            System.out.printf("%-20s :%d%n", key, map.get(key));
+        }
+    }
+}
